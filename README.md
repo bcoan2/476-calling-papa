c2da930a382207a9896b251a430452027c73f132
bcoan2 <122556056+bcoan2@users.noreply.github.com>
2024-02-20 17:01:37 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# solid-fishstick-snowcats but make it papa
+# solid-fishstick-snowcats
+Dr. Cumbie (yet again) re-staring a class repo for CIS 486 Spring '24 
--- app.js
+++ app.js
@@ -3,17 +3,16 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5500;
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
+const bodyParser = require('body-parser')
 // set the view engine to ejs
 let path = require('path');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // use res.render to load up an ejs view file
 
 let myTypeServer = "9️⃣ The Peacemaker ✌🏻";
-
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(process.env.URI, {
@@ -23,19 +22,39 @@ const client = new MongoClient(process.env.URI, {
     deprecationErrors: true,
   }
 });
-
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+    const result = await client.db("papa-database").collection("papa-collection").find().toArray();
+    console.log("papadatabase:" , result);
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    return result;
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
+//run().catch(console.dir);
+
+app.get('/read', async (req,res) => {
+
+  let myResultServer = await run(); 
+
+  console.log("myResultServer:", myResultServer);
+
+
+
+  res.render('index', {
+    myTypeClient: myResultServer,
+    myResultClient: myResultServer
+
+  });
+
+
+}); 
 run().catch(console.dir);
 
 
@@ -56,8 +75,6 @@ app.get('/send', function (req, res) {
     res.send('Hello World from Express <br><a href="/">home</a>')
 })
 
-// app.listen(3000)
-
 app.listen(port, () => {
-  console.log(`nov app listening on port ${port}`)
+console.log(`papa app listening on port ${port}`)
 })
