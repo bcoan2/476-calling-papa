8fda19c2a8027db1d2508fd71c26c0387a48df90
Brody Coan <bcoan2@una.edu>
2024-02-20 22:38:11 -0600

    this be it

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5500;
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const bodyParser = require('body-parser')
+// const bodyParser = require('');
+
 // set the view engine to ejs
 let path = require('path');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(bodyParser.urlencoded({ extended: true }))
 
 // use res.render to load up an ejs view file
 
-let myTypeServer = "8 The Challenger";
+let myTypeServer = "Type 8: The Challenger";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(process.env.URI, {
@@ -22,42 +22,48 @@ const client = new MongoClient(process.env.URI, {
     deprecationErrors: true,
   }
 });
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
     const result = await client.db("papa-database").collection("papa-collection").find().toArray();
-    console.log("papadatabase:" , result);
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    console.log("papa-database result: ", result);
+
     return result;
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
+//run().catch(console.dir);
 
-
-run().catch(console.dir);
 app.get('/read', async (req,res) => {
-  let myResultServer = await run(); 
 
-  console.log("myResultServer:", myResultServer);
+  let dbResult = await run();
+  console.log("myResults:", dbResult[0].userName);
+
+
+
 
   res.render('index', {
     myTypeClient: myTypeServer,
-    myResultClient: myResultServer,
+    myView: dbResult
+
   });
-}); 
-run().catch(console.dir);
+
+});
 
 
 
 app.get('/', function(req, res) {
 
   res.render('index', {
-   
     myTypeClient: myTypeServer 
 
   });
@@ -70,6 +76,84 @@ app.get('/send', function (req, res) {
     res.send('Hello World from Express <br><a href="/">home</a>')
 })
 
+// app.listen(3000)
+
 app.listen(port, () => {
-console.log(`papa app listening on port ${port}`)
+  console.log(`papa app listening on port ${port}`)
 })
+
+// require('dotenv').config()
+// const express = require('express')
+// const app = express()
+// const port = process.env.PORT || 5500;
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const bodyParser = require('body-parser')
+// // set the view engine to ejs
+// let path = require('path');
+// app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+// app.use(bodyParser.urlencoded({ extended: true }))
+
+// // use res.render to load up an ejs view file
+
+// let myTypeServer = "8 The Challenger";
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(process.env.URI, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     const result = await client.db("papa-database").collection("papa-collection").find().toArray();
+//     console.log("papadatabase:" , result);
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//     return result;
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+
+
+// run().catch(console.dir);
+// app.get('/read', async (req,res) => {
+//   let myResultServer = await run(); 
+
+//   console.log("myResultServer:", myResultServer);
+
+//   res.render('index', {
+//     myTypeClient: myTypeServer,
+//     myResultClient: myResultServer,
+//   });
+// }); 
+// run().catch(console.dir);
+
+
+
+// app.get('/', function(req, res) {
+
+//   res.render('index', {
+   
+//     myTypeClient: myTypeServer 
+
+//   });
+  
+// });
+
+
+// app.get('/send', function (req, res) {
+  
+//     res.send('Hello World from Express <br><a href="/">home</a>')
+// })
+
+// app.listen(port, () => {
+// console.log(`papa app listening on port ${port}`)
+// })
